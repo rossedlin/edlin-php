44cbdeb03e58c10875054c243976ce74f4f52446
Ross Edlin <rossedlin@gmail.com>
2015-12-26 19:54:37 +0000

    registry updates, cleaning up

--- library/cryslo/registry.php
+++ library/cryslo/registry.php
@@ -9,39 +9,19 @@ namespace Cryslo;
 
 final class Registry
 {
-    private static $instance;
-    private static $data = array();
-
-    public function __construct()
-    {
-        self::$instance = $this;
-    }
-
-    public static function instance()
-    {
-        return self::$instance;
-    }
-
-    public static function set($key, $value)
-    {
-        self::$data[$key] = $value;
-    }
-
-    //**************************************************************************************************************
-    // getters
-    //**************************************************************************************************************
+    /** @var Db  */
+    private static $db;
 
     /**
      * @return Db
      */
     public static function getDb()
     {
-        if (isset(self::$data['db']))
+        if (!isset(self::$db))
         {
-            return self::$data['db'];
+            self::$db = new Db(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
         }
 
-        self::set('db', new Db(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE));
-        return self::$data['db'];
+        return self::$db;
     }
 }
