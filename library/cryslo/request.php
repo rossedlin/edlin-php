a7432c89b25f03a26cb2bf4710181cfc881564a3
Ross Edlin <rossedlin@gmail.com>
2015-12-26 13:24:50 +0000

    request get/post validation

--- library/cryslo/request.php
+++ library/cryslo/request.php
@@ -9,9 +9,40 @@ namespace Cryslo;
 
 class Request
 {
+    /**
+     * Returns a validated post result if set, else the default
+     *
+     * @param $key
+     * @param bool $default
+     * @return string
+     */
+    static public function post($key, $default = false)
+    {
+        if (isset($_POST[$key])) return self::_validate($_POST[$key]);
+        return self::_validate($default);
+    }
+
+    /**
+     * Returns a validated get result if set, else the default
+     *
+     * @param $key
+     * @param bool $default
+     * @return string
+     */
     static public function get($key, $default = false)
     {
-        if (isset($_GET[$key])) return $_GET[$key];
-        return $default;
+        if (isset($_GET[$key])) return self::_validate($_GET[$key]);
+        return self::_validate($default);
+    }
+
+    /**
+     * Validates the data passed
+     *
+     * @param $data
+     * @return string
+     */
+    static private function _validate($data)
+    {
+        return strip_tags(trim($data));
     }
 }
