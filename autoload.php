3b26d32c09e5d8278e4cea4b56b9d92af550875f
rosse <rossedlin@gmail.com>
2016-02-10 18:21:30 +0000

    v1.2 update, small refactors Allow db without database

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+return array
+(
+    //Object
+    'Cryslo\Object\_Object' => 'library/cryslo/object/_object.php',
+    'Cryslo\Object\Query'   => 'library/cryslo/object/query.php',
+
+    //Library
+    'Cryslo\_Sql'           => 'library/cryslo/_sql.php',
+    'Cryslo\Db'             => 'library/cryslo/db.php',
+    'Cryslo\Directory'      => 'library/cryslo/directory.php',
+    'Cryslo\Mysqli'         => 'library/cryslo/mysqli.php',
+    'Cryslo\Registry'       => 'library/cryslo/registry.php',
+);
