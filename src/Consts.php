c972ad9faf3b26d1cd6fd254afec8fbe9fc58b5d
rossedlin <rossedlin@gmail.com>
2016-09-18 16:53:46 +0100

    add consts library

---
+++ src/Consts.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cryslo\Core;
+
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 18/09/2016
+ * Time: 16:48
+ */
+class Consts
+{
+	const CRLF = "\r\n";
+	const LF = "\n";
+}
