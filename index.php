32507248f362a859b9185d3b2051a1bf30bf0428
rossedlin <rossedlin@gmail.com>
2017-02-16 15:43:00 +0000

    remove index

--- index.php
+++ index.php
@@ -7,3 +7,18 @@
  */
 
 require __DIR__ . '/vendor/autoload.php';
+
+use \Cryslo\Core\Email;
+
+$to   = [
+	'rossedlin@gmail.com',
+	'rossedlin@hotmail.com',
+];
+$from = [
+	'noreply@cryslo.com' => 'Cryslo Team',
+];
+
+$subject = "Test Email";
+$message = "Hello World";
+
+pre(Email::sendHtml($to, $from, $subject, Email::getTemplate()));
