7a26989c1bbdc03f387bbfc3b0543e63b6884e09
Ross Edlin <rossedlin@gmail.com>
2017-09-09 18:08:42 +0700

    rename dir

---
+++ src/api_/CheckPulse.php
@@ -0,0 +1,33 @@
+<?php
+namespace Cryslo\Api;
+
+use \Cryslo\Core;
+
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 25/01/2017
+ * Time: 19:04
+ *
+ * Class CheckPulse
+ * @package Cryslo\Core\Api
+ */
+class CheckPulse
+{
+	const URL = "check-pulse/";
+
+	/**
+	 * @param $code
+	 *
+	 * @return bool
+	 */
+	public static function isOnline($code)
+	{
+		if (Core\Api::get(self::URL . $code)->getPayload()->online)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+}
