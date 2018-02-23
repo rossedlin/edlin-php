b8843df297a31b4d746bf51395347f706bea3f97
Ross Edlin <rossedlin@gmail.com>
2018-02-23 17:14:13 +0000

    move time to enums

---
+++ src/Enums/Time.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cryslo\Enums;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 26/01/2017
+ * Time: 11:46
+ *
+ * Class Time
+ * @package Cryslo\Enums
+ */
+class Time
+{
+    const MINUTE     = 60;
+    const HOUR       = 3600;
+    const DAY        = 86400;
+    const WEEK       = 604800;
+    const FOUR_WEEKS = 2419200;
+    const YEAR       = 31536000;
+}
