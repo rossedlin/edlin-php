75e6e3c87d5f2de4e6f14b538dc091acbc497860
Ross Edlin <rossedlin@gmail.com>
2018-02-23 16:03:35 +0000

    add - cli enums

---
+++ src/Enums/Cli.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Cryslo\Enums;
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 23/02/18
+ * Time: 15:26
+ */
+
+class Cli
+{
+    const _Close = "\033[0m";
+
+    const White  = "\033[0;30m";
+    const Red    = "\033[0;31m";
+    const Green  = "\033[0;32m";
+    const Yellow = "\033[0;33m";
+    const Blue   = "\033[0;34m";
+    const Purple = "\033[0;35m";
+    const Cyan   = "\033[0;36m";
+    const Gray   = "\033[0;37m";
+
+    const Bold_White  = "\033[1;30m";
+    const Bold_Red    = "\033[1;31m";
+    const Bold_Green  = "\033[1;32m";
+    const Bold_Yellow = "\033[1;33m";
+    const Bold_Blue   = "\033[1;34m";
+    const Bold_Purple = "\033[1;35m";
+    const Bold_Cyan   = "\033[1;36m";
+    const Bold_Gray   = "\033[1;37m";
+}
