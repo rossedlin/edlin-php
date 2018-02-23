e52be8180c08d79810b0861c20fb3fd1c1aa6640
Ross Edlin <rossedlin@gmail.com>
2018-02-23 17:18:18 +0000

    add - Request test

---
+++ tests/Core/RequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cryslo\Core;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 23/02/18
+ * Time: 17:09
+ */
+
+/**
+ * Class RequestTest
+ * @package Cryslo\Core
+ * @covers  Request
+ */
+final class RequestTest extends TestCase
+{
+    /**
+     * Testing Adding Numbers
+     */
+    public function testGetFromArray()
+    {
+        //todo
+        $this->assertEquals(1, 2);
+    }
+}
