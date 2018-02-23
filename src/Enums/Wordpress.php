3c409e07eb1a730dfd501b5e49866a84d7d07ed3
Ross Edlin <rossedlin@gmail.com>
2018-02-23 17:19:12 +0000

    add - wordpress enums

---
+++ src/Enums/Wordpress.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cryslo\Enums;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 23/02/18
+ * Time: 17:16
+ *
+ * Class Wordpress
+ * @package Cryslo\Enums
+ */
+class Wordpress
+{
+    const DATE_FORMAT = 'jS F Y';
+}
