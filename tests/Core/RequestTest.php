75bbb0b575799aed5ef667f7e046400dc52262cc
Ross Edlin <rossedlin@gmail.com>
2018-03-12 11:28:40 +0000

    add - rest getFromArray

--- tests/Core/RequestTest.php
+++ tests/Core/RequestTest.php
@@ -26,7 +26,34 @@ final class RequestTest extends TestCase
      */
     public function testGetFromArray()
     {
-        //todo
-        $this->assertEquals(1, 2);
+        /**
+         * Int Key
+         */
+        $data = [
+            'zero',
+            'one',
+            'two',
+            'three',
+        ];
+
+        $this->assertEquals(Request::getFromArray($data, 0), 'zero');
+        $this->assertEquals(Request::getFromArray($data, 1), 'one');
+        $this->assertEquals(Request::getFromArray($data, 2), 'two');
+        $this->assertEquals(Request::getFromArray($data, 3), 'three');
+
+        /**
+         * String Key
+         */
+        $data = [
+            'zero' => 'zero-123',
+            'one' => 'one-123',
+            'two' => 'two-123',
+            'three' => 'three-123',
+        ];
+
+        $this->assertEquals(Request::getFromArray($data, 'zero'), 'zero-123');
+        $this->assertEquals(Request::getFromArray($data, 'one'), 'one-123');
+        $this->assertEquals(Request::getFromArray($data, 'two'), 'two-123');
+        $this->assertEquals(Request::getFromArray($data, 'three'), 'three-123');
     }
 }
