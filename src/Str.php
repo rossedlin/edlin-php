5993fcbc053f5fe20cb30ec0b0f360a68faa783d
Ross Edlin <rossedlin@gmail.com>
2018-03-15 16:58:28 +0000

    add - cacheFriendlyKey

---
+++ src/Str.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cryslo;
+
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 15/03/18
+ * Time: 16:52
+ */
+class Str
+{
+    /**
+     * @param $key
+     * @return string
+     */
+    public static function cacheFriendlyKey($key)
+    {
+        return trim(preg_replace('/[^a-zA-Z0-9.]+/', '-', $key), '-');
+    }
+}
