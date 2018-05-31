eb3d1cf54c7386b65d821589fe6b7c7050d65ff8
Ross Edlin <rossedlin@gmail.com>
2018-05-31 11:32:55 +0100

    rename yearsFrom

--- tests/Core/DateTest.php
+++ tests/Core/DateTest.php
@@ -22,33 +22,33 @@ final class DateTest extends TestCase
      *
      * @throws CrysloException
      */
-    public function testYearsFrom()
+    public function testGetYearsFrom()
     {
         /**
          * Full Dates
          */
-        $this->assertEquals(0, Date::yearsFrom(strtotime("2018-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(1, Date::yearsFrom(strtotime("2017-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(2, Date::yearsFrom(strtotime("2016-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(3, Date::yearsFrom(strtotime("2015-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(6, Date::yearsFrom(strtotime("2012-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(17, Date::yearsFrom(strtotime("2001-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(18, Date::yearsFrom(strtotime("2000-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(19, Date::yearsFrom(strtotime("1999-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(48, Date::yearsFrom(strtotime("1970-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(218, Date::yearsFrom(strtotime("1800-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(1518, Date::yearsFrom(strtotime("500-01-01"), strtotime("2018-02-01")));
-        $this->assertEquals(2018, Date::yearsFrom(strtotime("0000-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(0, Date::getYearsFrom(strtotime("2018-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(1, Date::getYearsFrom(strtotime("2017-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(2, Date::getYearsFrom(strtotime("2016-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(3, Date::getYearsFrom(strtotime("2015-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(6, Date::getYearsFrom(strtotime("2012-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(17, Date::getYearsFrom(strtotime("2001-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(18, Date::getYearsFrom(strtotime("2000-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(19, Date::getYearsFrom(strtotime("1999-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(48, Date::getYearsFrom(strtotime("1970-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(218, Date::getYearsFrom(strtotime("1800-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(1518, Date::getYearsFrom(strtotime("500-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(2018, Date::getYearsFrom(strtotime("0000-01-01"), strtotime("2018-02-01")));
 
         /**
          * Partial Dates
          */
-        $this->assertEquals(18, Date::yearsFrom(strtotime("00-01-01"), strtotime("2018-02-01")));
+        $this->assertEquals(18, Date::getYearsFrom(strtotime("00-01-01"), strtotime("2018-02-01")));
 
         /**
          * Older Full Dates
          */
-        $this->assertEquals(29, Date::yearsFrom(strtotime("1970-01-01"), strtotime("1999-02-01")));
-        $this->assertEquals(29, Date::yearsFrom(strtotime("2001-01-01"), strtotime("1999-02-01")));
+        $this->assertEquals(29, Date::getYearsFrom(strtotime("1970-01-01"), strtotime("1999-02-01")));
+        $this->assertEquals(29, Date::getYearsFrom(strtotime("2001-01-01"), strtotime("1999-02-01")));
     }
 }
