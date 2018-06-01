68435e67eac7b5bbd7d2cadda0f9b6fd34e9c581
Ross Edlin <rossedlin@gmail.com>
2018-06-01 12:54:36 +0100

    make PS2

--- src/Core/Request.php
+++ src/Core/Request.php
@@ -1,154 +1,186 @@
 <?php
+
 namespace Cryslo\Core;
+
 /**
  * Created by PhpStorm.
- * User: Ross Edlin
+ *
+ * @author  Ross Edlin <contact@rossedlin.com>
+ *
  * Date: 07/11/2015
  * Time: 19:45
+ *
+ * Class Request
+ * @package Cryslo\Core
  */
-
 class Request
 {
-	/**
-	 * Returns a validated array result if set, else the default
-	 *
-	 * @param array $array
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return mixed
-	 */
-	public static function getFromArray(array &$array, $key, $default = false)
-	{
-		if (isset($array[$key])) return self::_clean($array[$key]);
-		return self::_clean($default);
-	}
+    /**
+     * Returns a validated array result if set, else the default
+     *
+     * @param array $array
+     * @param       $key
+     * @param bool  $default
+     *
+     * @return mixed
+     */
+    public static function getFromArray(array &$array, $key, $default = false)
+    {
+        if (isset($array[$key])) {
+            return self::clean($array[$key]);
+        }
 
-	/**
-	 * Returns a validated post result if set, else the default
-	 *
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return string
-	 */
-	static public function post($key, $default = false)
-	{
-		if (isset($_POST[$key])) return self::_clean($_POST[$key]);
-		return self::_clean($default);
-	}
+        return self::clean($default);
+    }
 
-	/**
-	 * Returns a validated get result if set, else the default
-	 *
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return string
-	 */
-	static public function get($key, $default = false)
-	{
-		if (isset($_GET[$key])) return self::_clean($_GET[$key]);
-		return self::_clean($default);
-	}
+    /**
+     * Returns a validated post result if set, else the default
+     *
+     * @param      $key
+     * @param bool $default
+     *
+     * @return string
+     */
+    public static function post($key, $default = false)
+    {
+        if (isset($_POST[$key])) {
+            return self::clean($_POST[$key]);
+        }
 
-	/**
-	 * Returns a validated server result if set, else the default
-	 *
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return array|string
-	 */
-	static public function server($key, $default = false)
-	{
-		if (isset($_SERVER[$key])) return self::_clean($_SERVER[$key]);
-		return self::_clean($default);
-	}
+        return self::clean($default);
+    }
 
-	/**
-	 * Returns a validated cookie result if set, else the default
-	 *
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return array|string
-	 */
-	static public function cookie($key, $default = false)
-	{
-		if (isset($_COOKIE[$key])) return self::_clean($_COOKIE[$key]);
-		return self::_clean($default);
-	}
+    /**
+     * Returns a validated get result if set, else the default
+     *
+     * @param      $key
+     * @param bool $default
+     *
+     * @return string
+     */
+    public static function get($key, $default = false)
+    {
+        if (isset($_GET[$key])) {
+            return self::clean($_GET[$key]);
+        }
 
-	/**
-	 * Returns a validated files result if set, else the default
-	 *
-	 * @param $key
-	 * @param bool $default
-	 *
-	 * @return array|string
-	 */
-	static public function files($key, $default = false)
-	{
-		if (isset($_FILES[$key])) return self::_clean($_FILES[$key]);
-		return self::_clean($default);
-	}
+        return self::clean($default);
+    }
 
-	/**
-	 * Cleans the data up
-	 *
-	 * @param $data
-	 *
-	 * @return array|string
-	 */
-	private static function _clean($data)
-	{
-		if ($data === false) return false;
-		if ($data === true) return true;
+    /**
+     * Returns a validated server result if set, else the default
+     *
+     * @param      $key
+     * @param bool $default
+     *
+     * @return array|string
+     */
+    public static function server($key, $default = false)
+    {
+        if (isset($_SERVER[$key])) {
+            return self::clean($_SERVER[$key]);
+        }
 
-		if (is_array($data))
-		{
-			foreach ($data as $key => $value)
-			{
-				unset($data[$key]);
-				$data[self::_clean($key)] = self::_clean($value);
-			}
-		}
-		else
-		{
-			$data = htmlspecialchars($data, ENT_COMPAT, 'UTF-8');
-		}
+        return self::clean($default);
+    }
 
-		return $data;
-	}
+    /**
+     * Returns a validated cookie result if set, else the default
+     *
+     * @param      $key
+     * @param bool $default
+     *
+     * @return array|string
+     */
+    public static function cookie($key, $default = false)
+    {
+        if (isset($_COOKIE[$key])) {
+            return self::clean($_COOKIE[$key]);
+        }
 
-	/**
-	 * Check if we have post data
-	 *
-	 * @return bool
-	 */
-	public static function isPost()
-	{
-		return (self::server('REQUEST_METHOD') === 'POST');
-	}
+        return self::clean($default);
+    }
 
-	/**
-	 * @param string $type
-	 *
-	 * @return bool
-	 */
-	public static function isValidType($type)
-	{
-		switch ((string)$type)
-		{
-			case 'GET':
-				return true;
-			case 'POST':
-				return true;
-			case 'PUT':
-				return true;
-		}
+    /**
+     * Returns a validated files result if set, else the default
+     *
+     * @param      $key
+     * @param bool $default
+     *
+     * @return array|string
+     */
+    public static function files($key, $default = false)
+    {
+        if (isset($_FILES[$key])) {
+            return self::clean($_FILES[$key]);
+        }
 
-		return false;
-	}
+        return self::clean($default);
+    }
+
+    /**
+     * Cleans the data up
+     *
+     * @param $data
+     *
+     * @return array|string
+     */
+    private static function clean($data)
+    {
+        /**
+         * Core values
+         */
+        if ($data === true) {
+            return true;
+        }
+        if ($data === false) {
+            return false;
+        }
+        if ($data === null) {
+            return null;
+        }
+
+        /**
+         * Filter through
+         */
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                unset($data[$key]);
+                $data[self::clean($key)] = self::clean($value);
+            }
+        } else {
+            $data = htmlspecialchars($data, ENT_COMPAT, 'UTF-8');
+        }
+
+        return $data;
+    }
+
+    /**
+     * Check if we have post data
+     *
+     * @return bool
+     */
+    public static function isPost()
+    {
+        return (self::server('REQUEST_METHOD') === 'POST');
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return bool
+     */
+    public static function isValidType($type)
+    {
+        switch ((string)$type) {
+            case 'GET':
+                return true;
+            case 'POST':
+                return true;
+            case 'PUT':
+                return true;
+        }
+
+        return false;
+    }
 }
