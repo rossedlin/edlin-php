7a2a09b1c4789a41d276980d624b173fea0c358c
Ross Edlin <rossedlin@gmail.com>
2018-06-01 18:18:10 +0100

    add simple xml

--- src/Core/Xml.php
+++ src/Core/Xml.php
@@ -13,10 +13,33 @@ namespace Cryslo\Core;
  * Class Xml
  * @package Cryslo\Core
  */
+/**
+ * Created by PhpStorm.
+ *
+ * User: rossedlin
+ * Contact: <contact@rossedlin.com>
+ *
+ * Date: 22/02/18
+ * Time: 17:42
+ */
+
 class Xml
 {
     /**
      * @param $raw
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public static function simpleToArray($raw): array
+    {
+        $obj  = simplexml_load_string($raw);
+        $json = json_encode($obj);
+        return json_decode($json, true);
+    }
+
+    /**
+     * @param string $xmlString
      *
      * @return array
      * @throws \Exception
---
+++ tests/Core/XmlTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Cryslo\Tests\Core;
+
+use PHPUnit\Framework\TestCase;
+use Cryslo\Core\Xml;
+
+/**
+ * Created by PhpStorm.
+ *
+ * @author  Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 01/06/18
+ * Time: 18:03
+ *
+ * Class XmlTest
+ * @package Cryslo\Tests\Core
+ * @covers  \Cryslo\Core\Xml
+ */
+final class XmlTest extends TestCase
+{
+    /**
+     * @covers \Cryslo\Core\Xml::simpleToArray
+     *
+     * @throws \Exception
+     */
+    public function testSimpleToArray()
+    {
+        $this->assertEquals(
+            require_once(__DIR__ . '/../../xml/test/simple.php'),
+            Xml::simpleToArray(file_get_contents(__DIR__ . '/../../xml/test/simple.xml'))
+        );
+    }
+}
