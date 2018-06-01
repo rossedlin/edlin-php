630ae798f1ebf94fd9ad505fa7e28684091ab730
Ross Edlin <rossedlin@gmail.com>
2018-06-01 17:38:38 +0100

    85% unit test

--- src/Core/Str.php
+++ src/Core/Str.php
@@ -42,7 +42,9 @@ class Str
      */
     public static function endsWith($haystack, $needle)
     {
-        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
+        return $needle === "" || (
+                ($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false
+            );
     }
 
     /**
