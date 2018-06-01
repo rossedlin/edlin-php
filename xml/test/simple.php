72926d08227df2ee7df463e289d582caf7d80130
Ross Edlin <rossedlin@gmail.com>
2018-06-01 18:18:10 +0100

    add simple xml

---
+++ xml/test/simple.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 01/06/18
+ * Time: 18:15
+ */
+
+return [
+    'to'      => 'Tove',
+    'from'    => 'Jani',
+    'heading' => 'Reminder',
+    'body'    => 'Don\'t forget me this weekend!',
+];
