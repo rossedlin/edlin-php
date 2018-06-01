e19b7230b069a78baa3fba9a490f9065aa120e17
Ross Edlin <rossedlin@gmail.com>
2018-06-01 17:56:42 +0100

    100% unit test coverage

--- src/Core/Utils.php
+++ src/Core/Utils.php
@@ -4,64 +4,11 @@ namespace Cryslo\Core;
 
 class Utils
 {
-     /**
-     * @param mixed      $object
-     * @param string     $key
-     * @param bool|mixed $default
-     *
-     * @return bool
-     */
-    public static function getMethodObject(&$object, $key, $default = false)
-    {
-        if (method_exists($object, $key)) {
-            return $object->$key();
-        }
-
-        return $default;
-    }
-
-    /**
-     * @param mixed      $object
-     * @param string     $key
-     * @param bool|mixed $default
-     *
-     * @return bool
-     */
-    public static function getVarObject(&$object, $key, $default = false)
-    {
-        /** @var $object \stdClass */
-        if (isset($object->$key)) {
-            return $object->$key;
-        }
-
-        return $default;
-    }
-
-    /**
-     * @param $str
-     *
-     * @return string
-     */
-    public static function snakeCaseToPascalCase($str)
-    {
-        return ucfirst(self::snakeCaseToCamelCase($str));
-    }
-
-    /**
-     * @param $str
-     *
-     * @return mixed
-     */
-    public static function snakeCaseToCamelCase($str)
-    {
-        $func = create_function('$c', 'return strtoupper($c[1]);');
-        return preg_replace_callback('/_([a-z])/', $func, $str);
-    }
-
     /**
      * @param $path
      *
      * @return string
+     * @codeCoverageIgnore
      */
     public static function addVersionToCssFile($path)
     {
@@ -96,5 +43,26 @@ class Utils
         }
 
         return $object;
+    }
+
+    /**
+     * @param $str
+     *
+     * @return string
+     */
+    public static function snakeCaseToPascalCase($str)
+    {
+        return ucfirst(self::snakeCaseToCamelCase($str));
+    }
+
+    /**
+     * @param $str
+     *
+     * @return mixed
+     */
+    public static function snakeCaseToCamelCase($str)
+    {
+        $func = create_function('$c', 'return strtoupper($c[1]);');
+        return preg_replace_callback('/_([a-z])/', $func, $str);
     }
 }
