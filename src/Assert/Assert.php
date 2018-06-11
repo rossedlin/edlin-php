5257948362d9f545485f1caaee87ebed519270bc
Ross Edlin <rossedlin@gmail.com>
2018-06-11 15:25:16 +0100

    add core assert lib

---
+++ src/Assert/Assert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cryslo\Assert;
+
+/**
+ * Created by PhpStorm.
+ *
+ * @author Ross Edlin <contact@rossedlin.com>
+ *
+ * Date: 11/06/18
+ * Time: 14:34
+ */
+
+class Assert
+{
+    /**
+     * @param $array
+     * @return ValidateArray
+     */
+    public static function validateArray($array)
+    {
+        return new ValidateArray($array);
+    }
+}
