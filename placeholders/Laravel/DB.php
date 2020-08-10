6c7270cf33e259312225fd01ba841c10a7d61225
Ross Edlin <rossedlin@gmail.com>
2020-08-10 14:49:00 +0700

    Add placeholder laravel class for \DB

---
+++ placeholders/Laravel/DB.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @author Ross Edlin <contact@rossedlin.com>
+ * Date: 2020-08-10
+ * Time: 12:38
+ */
+
+if (false) {
+
+    /**
+     * Class DB
+     *
+     * @method static select($query, $bindings = array(), $useReadPdo = true): array
+     * @method static raw($value): \Illuminate\Database\Query\Expression
+     */
+    class DB
+    {
+
+    }
+}
