b6c65c320562fb61d8d7be51ae06a5e400932dbb
Ross Edlin <rossedlin@gmail.com>
2021-09-19 00:17:58 +0700

    Add Checksum::validateDirectory

---
+++ src/Exceptions/ChecksumException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Ross Edlin <contact@rossedlin.com>
+ * Date: 19/09/2021
+ * Time: 00:15
+ */
+
+namespace Edlin\Exceptions;
+
+class ChecksumException extends \Exception
+{
+
+}
