ebd7423ab25348b8827043dcf88490c6a56fe7ef
Ross Edlin <rossedlin@gmail.com>
2024-11-20 14:16:34 +0000

    Update Build Process

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-sudo rm -R vendor
-docker run -v $PWD:/var/www -vedlin_php_composer:/root/.composer rossedlin/centos-apache-php:7.1 composer update
+#
+# Cleanup
+#
+rm -Rf vendor
+
+#
+# Build
+#
+docker compose run --rm web composer update
